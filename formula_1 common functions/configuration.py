f30441b8c7e7a8cb686fd432c8bfb8ca3f1142fe
sowmiya12345 <56109382+sowmiya12345@users.noreply.github.com>
2022-02-06 23:20:51 +0530

    Added common functions and configuration notebooks

---
+++ formula_1 common functions/configuration.py
@@ -0,0 +1,9 @@
+# Databricks notebook source
+input_folder_path = "/FileStore/tables/formula1_source_files"
+processed_folder_path = "/FileStore/tables/processed"
+presentation_folder_path = "/FileStore/tables/presentation"
+
+
+
+# COMMAND ----------
+
